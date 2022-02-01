12cf56b3428f20bfc96b76eda3952a437853db48
NT <nturner@zetta.ai>
2022-02-01 16:55:35 -0500

    Adding a hyphen for readability

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ __version__ = "0.1.0"
 
 
 setup(
-    name='provenancetoolbox',
+    name='provenance-toolbox',
     version=__version__,
     description=('Some basic utilities to standardize the use'
                  ' of CloudVolume provenance files'),
     author='Nicholas Turner',
     author_email='nturner@zetta.ai',
-    url='https://github.com/ZettaAI/provenancetoolbox',
+    url='https://github.com/ZettaAI/provenance-toolbox',
     packages=setuptools.find_packages(),
     install_requires=['GitPython>=3.1.26', 'docker>=5.0.3',
                       'cloud-volume']
