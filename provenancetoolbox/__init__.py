0706efb482134debedb17b930e555df27842a5c7
NT <nturner@zetta.ai>
2022-02-01 20:33:56 -0500

    Exposing NoteType for cirrus-volume

--- provenancetoolbox/__init__.py
+++ provenancetoolbox/__init__.py
@@ -1,10 +1,11 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from . import process
 from .process import Process, PythonGithubEnv, DockerEnv
 from .process import logprocess, process_absent
 
 from . import notebook
+from .notebook import NoteType
 from .notebook import parsenotes, note_absent
 from .notebook import addmotivation, addresult, addgeneric
 
